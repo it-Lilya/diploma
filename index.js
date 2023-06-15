ffc264ce492d71f235d361d5ced0f034ec06503c
It_Lilya <ihneva@bk.ru>
2023-06-15 15:43:13 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './src/App';
-import reportWebVitals from './src/reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import product from './mock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App products={product}/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
