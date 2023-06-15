6ff3a6fc0e599d724eb34f9a5443bde4dcfa2a98
It_Lilya <ihneva@bk.ru>
2023-06-15 16:05:42 +0300

    fixed the problem

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './src/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import product from './mock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App products={product}/>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
